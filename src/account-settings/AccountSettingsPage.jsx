dcf165a5f4b9fc275010a815c53ce9bfbcc0c826
Cormilo <gitgitgithub@yandex.ru>
2025-02-12 15:53:53 +0300

    Поставил дефолтное значение языка на английский

--- src/account-settings/AccountSettingsPage.jsx
+++ src/account-settings/AccountSettingsPage.jsx
@@ -563,7 +563,7 @@ class AccountSettingsPage extends React.Component {
             name="siteLanguage"
             type="select"
             options={this.props.siteLanguageOptions.filter(option => option.value === 'en')}
-            value={this.props.siteLanguage.draft !== undefined ? this.props.siteLanguage.draft : this.context.locale}
+            value={this.props.siteLanguage.draft ?? this.context.locale ?? 'en'}
             renderValue={value => <strong>{value}</strong>}
             helpText={this.props.intl.formatMessage(messages['account.settings.field.site.language.help.text'])}
             isEditable={false}
