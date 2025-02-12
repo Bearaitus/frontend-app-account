df1ee3ec3d86cc0a989480eedfc277747320198c
Cormilo <gitgitgithub@yandex.ru>
2025-02-12 16:22:23 +0300

    value='English'

--- src/account-settings/AccountSettingsPage.jsx
+++ src/account-settings/AccountSettingsPage.jsx
@@ -563,7 +563,7 @@ class AccountSettingsPage extends React.Component {
             name="siteLanguage"
             type="select"
             options={this.props.siteLanguageOptions.filter(option => option.value === 'en')}
-            value='en'
+            value='English'
             renderValue={value => <strong>{value}</strong>}
             helpText={this.props.intl.formatMessage(messages['account.settings.field.site.language.help.text'])}
             isEditable={false}
